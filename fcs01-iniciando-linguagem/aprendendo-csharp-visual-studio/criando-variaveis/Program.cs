65ede2f5533bee325ed718b3067493fa0f6e11e4
WybsonSantana <wybson@hotmail.com>
2023-02-12 18:18:14 -0300

    fcs01 - aula 3 - Criando variáveis

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/criando-variaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2: criando variáveis");
+
+        int idade;
+
+        idade = 27;
+        Console.WriteLine("A idade de Fulano é " + idade);
+
+        idade = 27 - 5;
+        Console.WriteLine(idade);
+
+        idade = 5 * 2 - 6;
+        Console.WriteLine(idade);
+
+        idade = (5 - 2) * 3;
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
