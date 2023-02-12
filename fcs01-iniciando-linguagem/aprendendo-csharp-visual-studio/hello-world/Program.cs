e2067ff9110ccd306a0c2c063baf7e6a6fcbf3ec
WybsonSantana <wybson@hotmail.com>
2023-02-12 13:11:41 -0300

    fcs01 - aula 2 - Estrutura de diretórios do Visual Studio

--- fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/hello-world/Program.cs
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/hello-world/Program.cs
@@ -4,5 +4,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, world!");
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
     }
 }
