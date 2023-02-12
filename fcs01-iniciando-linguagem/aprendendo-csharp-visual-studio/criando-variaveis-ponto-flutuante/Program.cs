3e9ad65b97aa669cd9b50d68fc100ffff82068a6
WybsonSantana <wybson@hotmail.com>
2023-02-12 18:21:44 -0300

    fcs01 - aula 3 - Criando variáveis double

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/criando-variaveis-ponto-flutuante/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3: criando variáveis ponto flutuante");
+
+        double salario;
+        salario = 3000.10;
+        Console.WriteLine(salario);
+
+        salario = 3000;
+        Console.WriteLine(salario);
+
+        double numero;
+        numero = 7 / 5;
+        Console.WriteLine(numero);
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
