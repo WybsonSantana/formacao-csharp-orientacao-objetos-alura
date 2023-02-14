f824afa180e665d655b453c8489f138c6846fc2d
WybsonSantana <wybson@hotmail.com>
2023-02-13 23:41:35 -0300

    fcs01 - aula 5 - Trabalhando com if

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/condicionais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7: condicionais");
+
+        int idadeJoao = 16;
+        int quantidadeDePessoas = 2;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Entrada permitida!");
+        }
+        else
+        {
+            if (quantidadeDePessoas > 1)
+            {
+                Console.WriteLine("Pessoa acompanhada. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Entrada não permitida!");
+            }
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar");
+        Console.ReadLine();
+    }
+}
