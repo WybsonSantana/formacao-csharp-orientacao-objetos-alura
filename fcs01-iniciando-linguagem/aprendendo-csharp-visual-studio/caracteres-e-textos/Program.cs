d31e48dd86785e293f691cde4a5b553d3af1a765
WybsonSantana <wybson@hotmail.com>
2023-02-13 21:44:36 -0300

    fcs01 - aula 4 - Caracteres

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/caracteres-e-textos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5: caracteres e textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Pressione EnTER para finalizar!");
+        Console.ReadLine();
+    }
+}
