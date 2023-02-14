4cdb69dde9cec2bc441d9064b8ce1de4f463fe76
WybsonSantana <wybson@hotmail.com>
2023-02-13 23:00:05 -0300

    fcs01 - aula 4 - Variáveis guardam valores

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/atribuicoes-de-variaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 6: atribuições e variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
