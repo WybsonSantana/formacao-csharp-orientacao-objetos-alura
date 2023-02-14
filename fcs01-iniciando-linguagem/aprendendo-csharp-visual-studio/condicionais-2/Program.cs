3fc720d29f92a22409d60b482d289a7fe783999d
WybsonSantana <wybson@hotmail.com>
2023-02-14 08:41:35 -0300

    fcs01 - aula 5 - Variável booleana

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/condicionais-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 8: condicionais 2");
+
+        int idadeJoao = 16;
+        int quantidadeDePessoas = 2;
+
+        bool acompanhado = quantidadeDePessoas > 1;
+        bool grupo = false;
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Entrada permitida!");
+        }
+        else
+        {
+            Console.WriteLine("Entrada não permitida!");
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar");
+        Console.ReadLine();
+    }
+}
