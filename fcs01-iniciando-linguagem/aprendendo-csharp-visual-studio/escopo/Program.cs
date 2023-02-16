fef79aced85e45ebfc5498c4a5e89d686be32d78
WybsonSantana <wybson@hotmail.com>
2023-02-15 21:48:03 -0300

    fcs01 - aula 5 - Escopo

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/escopo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 9: escopo");
+
+        int idadeJoao = 16;
+        int quantidadeDePessoas = 2;
+
+        bool acompanhado = quantidadeDePessoas > 1;
+
+        string textoAdicional;
+
+        if (acompanhado == true)
+        {
+            textoAdicional = "João está acompanhado.";
+        }
+        else
+        {
+            textoAdicional = "João não está acompanhado.";
+        }
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Entrada permitida!");
+        }
+        else
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Entrada não permitida!");
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar");
+        Console.ReadLine();
+    }
+}
