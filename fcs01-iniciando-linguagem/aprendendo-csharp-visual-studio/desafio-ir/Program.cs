84931de80e286e1a3f7e9e07745a1a57cb7f38c6
WybsonSantana <wybson@hotmail.com>
2023-02-15 21:49:05 -0300

    fcs01 - aula 5 - Desafio: alíquota com ifs

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/desafio-ir/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 10: desafio IR");
+
+        double salario = 3300.0;
+
+        if (salario < 1903.99)
+        {
+            Console.WriteLine("Isento de IR");
+        }
+        else if (salario >= 1903.99 && salario <= 2826.65)
+        {
+            Console.WriteLine("Alícota de 7,5%");
+            Console.WriteLine("Dedução de até R$ 142,80");
+        }
+        else if (salario >= 2826.66 && salario <= 3751.05)
+        {
+            Console.WriteLine("Alícota de 15%");
+            Console.WriteLine("Dedução de até R$ 354,80");
+        }
+        else if (salario >= 3751.06 && salario <= 4664.08)
+        {
+            Console.WriteLine("Alícota de 22,5%");
+            Console.WriteLine("Dedução de até R$ 636,13");
+        }
+        else
+        {
+            Console.WriteLine("Alícota de 27,5%");
+            Console.WriteLine("Dedução de até R$ 869,36");
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar");
+        Console.ReadLine();
+    }
+}
