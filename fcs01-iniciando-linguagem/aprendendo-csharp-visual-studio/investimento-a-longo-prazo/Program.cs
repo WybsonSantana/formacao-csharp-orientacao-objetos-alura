7752036e79ea36b4acc2e62faa0ae60dfc79cfeb
WybsonSantana <wybson@hotmail.com>
2023-02-16 22:47:32 -0300

    fcs01 - aula 6 - Encadeando laços for

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/investimento-a-longo-prazo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13: investimento a longo prazo");
+
+        double investimento = 1000.0;
+        double fatorRendimento = 1.005;
+
+        for (int ano = 1; ano <= 5; ano++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
+            fatorRendimento += 0.001;
+        }
+
+        Console.WriteLine($"Após 5 anos de investimento você terá {investimento.ToString("c")}");
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
