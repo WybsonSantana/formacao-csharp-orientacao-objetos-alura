b24dbe6a9ed06c4a4aa7b28e5f41d08df267afba
WybsonSantana <wybson@hotmail.com>
2023-02-17 20:54:19 -0300

    fcs01 - aula 6 - A palavra chave break

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/encadeando-for/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 14: encadeando for");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        // Com break
+        for (int linha = 0; linha < 10; linha++)
+        {
+            for (int coluna = 0; coluna < 10; coluna++)
+            {
+                Console.Write("*");
+                if (coluna >= linha)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        // Sem break
+        for (int linha = 0; linha < 10; linha++)
+        {
+            for (int coluna = 0; coluna <= linha; coluna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar");
+        Console.ReadLine();
+    }
+}
