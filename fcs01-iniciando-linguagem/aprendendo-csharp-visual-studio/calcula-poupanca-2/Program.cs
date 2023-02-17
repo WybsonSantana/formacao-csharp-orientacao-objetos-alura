f6ef55cdc16e92e1b6a6be132c1c9ad876a3706e
WybsonSantana <wybson@hotmail.com>
2023-02-16 22:27:09 -0300

    fcs01 - aula 6 - Laço de repetição for

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/calcula-poupanca-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 12: calcula poupança 2");
+
+        /*
+        int mes = 1;
+
+        while (mes <= 12)
+        {
+            investimento += investimento * 0.005;
+
+            Console.WriteLine($"No mês {mes} você terá {investimento.ToString("c")}");
+
+            mes++;
+        }
+        */
+
+        double investimento = 1000.0;
+
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            investimento *= 1.005;
+
+            Console.WriteLine($"No mês {mes} você terá {investimento.ToString("c")}");
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
