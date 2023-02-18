a19aaf1e34c4dc71ecbedfc8a372a50a1d9dfde3
WybsonSantana <wybson@hotmail.com>
2023-02-17 21:22:41 -0300

    fcs01 - aula 6 - Desafio: múltiplos de 3

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/multiplos-de-tres/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 15: múltiplos de 3");
+
+        // Incrementando +3
+        for (int i = 0; i <= 100; i += 3)
+        {
+            if (i > 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.WriteLine();
+
+        // Divisível por 3
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
