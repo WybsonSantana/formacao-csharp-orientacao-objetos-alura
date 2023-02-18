0dd16263ba621777619d48059af9f9567b07af52
WybsonSantana <wybson@hotmail.com>
2023-02-17 22:05:26 -0300

    fcs01 - aula 6 - Desafio: fatorial

---
+++ fcs01-iniciando-linguagem/aprendendo-csharp-visual-studio/fatorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 16: fatorial");
+
+        int fatorial = 1;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine($"O fatorial de {i} é {fatorial}");
+        }
+
+        Console.WriteLine("Pressione ENTER para finalizar!");
+        Console.ReadLine();
+    }
+}
