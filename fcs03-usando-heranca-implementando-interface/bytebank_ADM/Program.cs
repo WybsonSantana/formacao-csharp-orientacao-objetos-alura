f96d0d111039ef324e8ba46ea79d1ff3b6968699
WybsonSantana <wybson@hotmail.com>
2023-02-21 18:28:18 -0300

    fcs03 - aula 1 - Classe funcionário

--- fcs03-usando-heranca-implementando-interface/bytebank_ADM/Program.cs
+++ fcs03-usando-heranca-implementando-interface/bytebank_ADM/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("bytebank_ADM");
+﻿using bytebank_ADM.Funcionarios;
+
+Funcionario fulano = new Funcionario();
+fulano.Nome = "Fulano de Tal";
+fulano.Cpf = "807.018.000-54";
+fulano.salario = 2000.0;
+
+Console.WriteLine(fulano.Nome);
+Console.WriteLine(fulano.GetBonificacao().ToString("c"));
