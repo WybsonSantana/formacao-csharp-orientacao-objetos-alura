4ac740c06642d5b72484d10b4476f1b789b14dd0
WybsonSantana <wybson@hotmail.com>
2023-02-21 22:36:19 -0300

    fcs03 - aula 1 - Sobrecarga de método

--- fcs03-usando-heranca-implementando-interface/bytebank_ADM/Program.cs
+++ fcs03-usando-heranca-implementando-interface/bytebank_ADM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitario;
 
 Funcionario fulano = new Funcionario();
 fulano.Nome = "Fulano de Tal";
@@ -7,3 +8,16 @@ fulano.salario = 2000.0;
 
 Console.WriteLine(fulano.Nome);
 Console.WriteLine(fulano.GetBonificacao().ToString("c"));
+
+Diretor belTrano = new Diretor();
+belTrano.Nome = "Bel Trano";
+belTrano.Cpf = "958.927.410-28";
+belTrano.salario = 5000.0;
+
+Console.WriteLine(belTrano.Nome);
+Console.WriteLine(belTrano.GetBonificacao().ToString("c"));
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+gerenciador.Registrar(fulano);
+gerenciador.Registrar(belTrano);
+Console.WriteLine($"\nTotal em bonificações; {gerenciador.TotalDeBonificacao.ToString("c")}");
