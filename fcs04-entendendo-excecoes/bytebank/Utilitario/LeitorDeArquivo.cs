cd96f35cf70281375bb8d84cb40513b5abefdb41
WybsonSantana <wybson@hotmail.com>
2023-02-25 10:21:11 -0300

    fcs04 - aula 5 - Preparando o ambiente

---
+++ fcs04-entendendo-excecoes/bytebank/Utilitario/LeitorDeArquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank.Utilitario
+{
+    public class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            this.Arquivo = arquivo;
+
+            // throw new FileNotFoundException();
+
+            Console.WriteLine($"Abrindo arquivo: {arquivo}");
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo a linha do arquivo...");
+
+            throw new IOException();
+
+            return "Linha do arquivo";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando o arquivo...");
+        }
+    }
+}
