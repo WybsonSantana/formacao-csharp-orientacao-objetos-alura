ae425fd1167de27eba52d3b2c188720432284648
WybsonSantana <wybson@hotmail.com>
2023-02-25 09:52:51 -0300

    fcs04 - aula 4 - StackTrace

--- fcs04-entendendo-excecoes/bytebank/Program.cs
+++ fcs04-entendendo-excecoes/bytebank/Program.cs
@@ -3,7 +3,7 @@ using bytebank.Exceptions;
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(278, "1213-4");
+    ContaCorrente conta1 = new ContaCorrente(0, "1213-4");
     conta1.Sacar(150.0);
     Console.WriteLine($"Saldo: {conta1.GetSaldo().ToString("c")}");
 }
@@ -11,6 +11,7 @@ catch (ArgumentException ex)
 {
     Console.WriteLine($"Parâmetro com erro: {ex.ParamName}");
     Console.WriteLine("Ops! Algo deu errado! Não é possível criar uma conta com agência menor ou igual a zero");
+    Console.WriteLine(ex.StackTrace);
     Console.WriteLine(ex.Message);
 }
 catch (SaldoInsuficienteException ex)
